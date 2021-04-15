3ba3d8a0f38f698ac2406364581da21e5cffe2eb
Emilio Pagnoca <emilio@hotmail.dk>
2021-04-14 23:21:23 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,11 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 class Response{
     public $images = array();
     public $title;
@@ -12,42 +18,32 @@ function getData($url){
     if($url) {
 
         if(explode('Restaurant_Review',$url)[0] !== 'https://www.tripadvisor.com.br/') {
-
-            echo "Você deve informar uma URL válida de restaurante do tripadvisor!";
-
+            throw new Exception("You need to inform an valid restaurant review URL of tripadvisor!");
         } else {
-
             $response = new Response();
-
             $html = file_get_contents($url);
 
-            //region Get restaurant images
+            #region Get restaurant images
             preg_match_all('|<img.*?src=[\'"](.*?)[\'"].*?data-mediaid=[\'"].*?[\'"].*?>|i', $html, $images);
-
             for($i=0; $i < count($images[0]); $i++){
-
                 $xpath = new DOMXPath(@DOMDocument::loadHTML($images[0][$i]));
                 $src = $xpath->evaluate("string(//img/@src)");
                 array_push($response->images, $src);
-
             }
-
             foreach (array_keys($response->images, 'https://static.tacdn.com/img2/x.gif') as $key) {
                 unset($response->images[$key]);
             }
-
             $response->images = array_values($response->images);
-            //endregion
+            #endregion
 
-            //region Get restaurant name
+            #region Get restaurant name
             preg_match('/<li class=\"breadcrumb\">(.*?)<\/li>/s', $html, $title);
-
             $xpath = new DOMXPath(@DOMDocument::loadHTML($title[0]));
             $node = $xpath->evaluate("string(//li/node())");
             $response->title = utf8_decode($node);
-            //endregion
+            #endregion
 
-            //region Get restaurant rates
+            #region Get restaurant rates
             preg_match('/<span property=\"count\">(.*?)<\/span>/s', $html, $rates);
 
             $xpath = new DOMXPath(@DOMDocument::loadHTML($rates[0]));
@@ -57,24 +53,19 @@ function getData($url){
             } else {
                 $response->rates = $node;
             }
-            //endregion
+            #endregion
 
-            //region Get restaurant rates
+            #region Get restaurant rates
             preg_match('/<span class=\"overallRating\">(.*?)<\/span>/s', $html, $average);
-
             $xpath = new DOMXPath(@DOMDocument::loadHTML($average[0]));
             $node = $xpath->evaluate("string(//span/node())");
             $response->average = $node;
-            //endregion
+            #endregion
 
             echo json_encode($response,JSON_PRETTY_PRINT);
-
         }
-
     } else {
-
-        echo "Por favor, informe uma URL do tripadvisor";
-
+        throw new Exception('Empty URL');
     }
 
 }
